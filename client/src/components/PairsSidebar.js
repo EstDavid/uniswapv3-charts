9d445bcc50d4c0d262755490d6fee98ec337b631
Vandenynas <stlo.subs@gmail.com>
2022-07-15 07:49:55 +0300

    Added pairsListsSelector

--- client/src/components/PairsSidebar.js
+++ client/src/components/PairsSidebar.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import PairsList from './PairsList';
+import { pairsListsSelector } from '../slices/pairsLists';
 
 const PairsSidebar = () => {
+    const {quoteSymbols} = useSelector(pairsListsSelector);
+
     return(
         <div className="flex-shrink-0 p-3 bg-white" style={{width: "280px"}}>
             <a href="/" className="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
@@ -9,59 +13,9 @@ const PairsSidebar = () => {
                 <span className="fs-5 fw-semibold">Pool Pairs</span>
             </a>
             <ul className="list-unstyled ps-0">
-                <li className="mb-1">
-                    <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-                        ETH pairs
-                    </button>
-                    <div className="collapse show" id="home-collapse">
-                        <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="#" className="link-dark rounded">Overview</a></li>
-                            <li><a href="#" className="link-dark rounded">Updates</a></li>
-                            <li><a href="#" className="link-dark rounded">Reports</a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li className="mb-1">
-                    <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-                        Stable coin pairs
-                    </button>
-                    <div className="collapse" id="dashboard-collapse">
-                        <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="#" className="link-dark rounded">Overview</a></li>
-                            <li><a href="#" className="link-dark rounded">Weekly</a></li>
-                            <li><a href="#" className="link-dark rounded">Monthly</a></li>
-                            <li><a href="#" className="link-dark rounded">Annually</a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li className="mb-1">
-                    <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-                        All pairs
-                    </button>
-                    <div className="collapse" id="orders-collapse">
-                        <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="#" className="link-dark rounded">New</a></li>
-                            <li><a href="#" className="link-dark rounded">Processed</a></li>
-                            <li><a href="#" className="link-dark rounded">Shipped</a></li>
-                            <li><a href="#" className="link-dark rounded">Returned</a></li>
-                        </ul>
-                    </div>
-                </li>
-                <PairsList listName="New Pairs" idNumber={5} />
-                <li className="border-top my-3"></li>
-                <li className="mb-1">
-                    <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
-                        Account
-                    </button>
-                    <div className="collapse" id="account-collapse">
-                        <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="#" className="link-dark rounded">New...</a></li>
-                            <li><a href="#" className="link-dark rounded">Profile</a></li>
-                            <li><a href="#" className="link-dark rounded">Settings</a></li>
-                            <li><a href="#" className="link-dark rounded">Sign out</a></li>
-                        </ul>
-                    </div>
-                </li>
+                {quoteSymbols.map((symbol, index) => {
+                    return <PairsList key={index} listName={`${symbol} pairs`} idNumber={index} listSymbol={symbol} />
+                })}
             </ul>
         </div>
     )

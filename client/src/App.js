9c2edeea31246d1ff5e22ba406ca2bfd5532924f
Vandenynas <stlo.subs@gmail.com>
2022-07-15 07:46:35 +0300

    Added Navbar, IndicatorCreator and PairsSidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React  from 'react';
+import Navbar from './components/Navbar';
 import PriceChart from './components/PriceChart';
+import PairsSidebar from './components/PairsSidebar';
+import IndicatorCreator from './components/IndicatorCreator';
 
 function App() {
   return (
     <div className="App">
-      {/* <PriceData /> */}
-      <PriceChart />
+      <Navbar />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-2">
+            <IndicatorCreator />
+          </div>
+          <div className="col-sm-8">
+            <PriceChart />
+          </div>
+          <div className="col-sm-2">
+            <PairsSidebar />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

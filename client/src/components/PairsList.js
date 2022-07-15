76fd8df1e1642279e099f2651caa4656987bc230
Vandenynas <stlo.subs@gmail.com>
2022-07-15 07:48:48 +0300

    Added main functionalities

--- client/src/components/PairsList.js
+++ client/src/components/PairsList.js
@@ -1,35 +1,66 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPriceData, priceDataSelector} from '../slices/priceData';
-  
+import { fetchPairsLists, filterInput, pairsListsSelector } from '../slices/pairsLists';
 
 const PairsList = (props) => {
     const dispatch = useDispatch();
 
     const {
         viewTimeframe
-      } = useSelector(priceDataSelector);
+    } = useSelector(priceDataSelector);
+
+    const {
+        pairsListLoading,
+        pairsListError,
+        pairsLists,
+        searchPairsLists,
+        searchInputs
+    } = useSelector(pairsListsSelector);
+
+    useEffect(() => {
+        dispatch(fetchPairsLists());
+    }, [dispatch]);
 
 
     const selectSymbol = (event) => {
         event.preventDefault();
-        console.log(event.target.text)
         dispatch(fetchPriceData(event.target.text, viewTimeframe));
     }
 
+    const filterSymbols = (event) => {
+        event.preventDefault();
+        dispatch(filterInput(
+            event.target.value,
+            props.listSymbol,
+            pairsLists[props.listSymbol]
+        ));
+    }
+
     return (
-        <li className="mb-1">
+        <li className="mb-1" key={props.idNumber}>
             <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target={`#dashboard-collapse-${props.idNumber}`} aria-expanded="false">
                 {props.listName}
             </button>
-            <div className="collapse" id={`dashboard-collapse-${props.idNumber}`}>
-                <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li><a href="#" className="link-dark rounded" onClick={selectSymbol}>AAVEWETH</a></li>
-                    <li><a href="#" className="link-dark rounded" onClick={selectSymbol}>WETHUSDC</a></li>
-                    <li><a href="#" className="link-dark rounded" onClick={selectSymbol}>WBTCUSDC</a></li>
-                    <li><a href="#" className="link-dark rounded" onClick={selectSymbol}>FRAXWETH</a></li>
-                </ul>
-            </div>
+            {pairsListLoading ? 
+                <h2>Loading pairs...</h2>
+            :
+                <div className="collapse" id={`dashboard-collapse-${props.idNumber}`}>
+                    <input 
+                        type="text"
+                        placeholder={`Search ${props.listSymbol} symbol...`}
+                        id={`search-${props.listSymbol.toLowerCase()}`}
+                        onChange={filterSymbols}
+                        value={searchInputs[props.listSymbol]} 
+                    />
+                    <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                        {searchPairsLists[props.listSymbol].map((symbol, index) => {
+                            return <li key={index}><a href="#" className="link-dark rounded" onClick={selectSymbol}>{symbol}</a></li>
+                        })}
+                    </ul>
+                </div>
+            }
+
         </li>
     )
 }

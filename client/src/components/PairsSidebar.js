60f2b16a2c5ce0ac94889cd45d42565c11c93c09
Vandenynas <stlo.subs@gmail.com>
2022-07-18 08:57:43 +0300

    Changed 'a' element for 'div' element

--- client/src/components/PairsSidebar.js
+++ client/src/components/PairsSidebar.js
@@ -8,10 +8,10 @@ const PairsSidebar = () => {
 
     return(
         <div className="flex-shrink-0 p-3 bg-white" style={{width: "280px"}}>
-            <a href="/" className="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+            <div className="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
                 <svg className="bi me-2" width="30" height="24"></svg>
                 <span className="fs-5 fw-semibold">Pool Pairs</span>
-            </a>
+            </div>
             <ul className="list-unstyled ps-0">
                 {quoteSymbols.map((symbol, index) => {
                     return <PairsList key={index} listName={`${symbol} pairs`} idNumber={index} listSymbol={symbol} />

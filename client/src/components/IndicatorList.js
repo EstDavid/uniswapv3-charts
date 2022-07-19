4adab902e4defcbf7045596f840b314afa4cfcbc
Vandenynas <stlo.subs@gmail.com>
2022-07-19 08:07:55 +0300

    Added color legend. Added disabling when data is loading

--- client/src/components/IndicatorList.js
+++ client/src/components/IndicatorList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {priceDataSelector, deleteMovingAverage} from '../slices/priceData';
+import {priceDataSelector, deleteMovingAverage, toggleVisibility} from '../slices/priceData';
 import {configureExistingIndicator, toggleDialog} from '../slices/indicatorConfig';
 import IndicatorSettings from './IndicatorSettings';
 
 const IndicatorList = () => {
     const dispatch = useDispatch();
-    const {priceObject, chartObject} = useSelector(priceDataSelector);
+    const {priceObject, chartObject, indicatorColors, loading} = useSelector(priceDataSelector);
 
     const handleDelete = (id) => {
         dispatch(deleteMovingAverage(id));
@@ -18,35 +18,45 @@ const IndicatorList = () => {
         dispatch(toggleDialog());
     }
 
-    return (
-        <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" style={{ width: "380 px" }}>
+    const handleVisibility = (id) => {
+        dispatch(toggleVisibility(id));
+    }
 
-            <div className="list-group list-group-flush border-bottom scrollarea">
-                {chartObject.series.map((dataObject) => {
+    return (
+        <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
+                {chartObject.series.map((dataObject, index) => {
                     if (dataObject.id > 1) {
                         return (
-                            <a key={dataObject.id} href="#" className="list-group-item list-group-item-action py-3 lh-tight">
+                            <div key={dataObject.id} className="list-group-item list-group-item-action py-3 lh-tight">
                                 <div className="d-flex w-100 align-items-center justify-content-between">
-                                    <strong className="mb-1">{dataObject.name}</strong>
+                                    <small style={{minWidth: "60px"}}>{dataObject.name}</small>
+                                    <i className="bi bi-circle-fill" style={{color: indicatorColors[index]}}></i>
                                     <button 
                                         className="btn"
                                         aria-label="Configure"
-                                        onClick={() => {handleConfigure(dataObject.id)}} 
+                                        onClick={() => {handleConfigure(dataObject.id)}}
+                                        disabled={loading}
                                     ><i className="bi bi-gear-fill"></i></button>
                                     <button 
+                                        className="btn"
+                                        aria-label="Configure"
+                                        onClick={() => {handleVisibility(dataObject.id)}}
+                                        disabled={loading}
+                                    ><i className={`bi bi-eye${dataObject.visible ? '' : '-slash'}-fill`}></i></button>
+                                    <button 
                                         type="button"
-                                        className="btn btn-close"
+                                        className="btn btn-close btn-sm"
                                         aria-label="Close"
+                                        disabled={loading}
                                         onClick={() => {handleDelete(dataObject.id)}} 
                                     ></button>
                                 </div>
                                 <IndicatorSettings buttonText="Update indicator" indicatorId={dataObject.id} />
-                            </a>
+                            </div>
                         )
 
                     }
                 })}
-            </div>
         </div>
     )
 }

94217584c2bb297d1e55ca520a00467e1a234057
Vandenynas <stlo.subs@gmail.com>
2022-07-20 06:50:18 +0300

    Added color selector and changed order of buttons in configuration menu

--- client/src/components/IndicatorList.js
+++ client/src/components/IndicatorList.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {priceDataSelector, deleteMovingAverage, toggleVisibility} from '../slices/priceData';
-import {configureExistingIndicator, toggleDialog} from '../slices/indicatorConfig';
+import {
+    priceDataSelector,
+    deleteMovingAverage,
+    toggleVisibility,
+    changeColor
+} from '../slices/priceData';
+import {
+    configureExistingIndicator,
+    toggleDialog,
+} from '../slices/indicatorConfig';
 import IndicatorSettings from './IndicatorSettings';
 
 const IndicatorList = () => {
@@ -22,6 +30,10 @@ const IndicatorList = () => {
         dispatch(toggleVisibility(id));
     }
 
+    const handleColorChange = (id, color) => {
+        dispatch(changeColor(id, color));
+    }
+
     return (
         <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
                 {chartObject.series.map((dataObject, index) => {
@@ -30,19 +42,35 @@ const IndicatorList = () => {
                             <div key={dataObject.id} className="list-group-item list-group-item-action py-3 lh-tight">
                                 <div className="d-flex w-100 align-items-center justify-content-between">
                                     <small style={{minWidth: "60px"}}>{dataObject.name}</small>
-                                    <i className="bi bi-circle-fill" style={{color: indicatorColors[index]}}></i>
                                     <button 
                                         className="btn"
-                                        aria-label="Configure"
-                                        onClick={() => {handleConfigure(dataObject.id)}}
+                                        aria-label="Configure color"
+                                        id={`color-selector${dataObject.id}`} data-bs-toggle="dropdown" aria-expanded="false"
                                         disabled={loading}
-                                    ><i className="bi bi-gear-fill"></i></button>
+                                    ><i className="bi bi-circle-fill" style={{color: dataObject.color}}></i></button>
+                                    <div className="dropdown-menu" aria-labelledby={`color-selector${dataObject.id}`}>
+                                        <div className="btn-group">
+                                            {indicatorColors.map((color, index) => {
+                                                return (
+                                                <button key={index} className="btn btn-link"
+                                                    onClick={() => {handleColorChange(dataObject.id, color)}}>
+                                                    <i className="bi bi-circle-fill" style={{color: color}}></i>
+                                                </button>)
+                                            })}
+                                        </div>
+                                    </div>
                                     <button 
                                         className="btn"
-                                        aria-label="Configure"
+                                        aria-label="Configure visibility"
                                         onClick={() => {handleVisibility(dataObject.id)}}
                                         disabled={loading}
                                     ><i className={`bi bi-eye${dataObject.visible ? '' : '-slash'}-fill`}></i></button>
+                                    <button 
+                                        className="btn"
+                                        aria-label="Configure settings"
+                                        onClick={() => {handleConfigure(dataObject.id)}}
+                                        disabled={loading}
+                                    ><i className="bi bi-gear-fill"></i></button>
                                     <button 
                                         type="button"
                                         className="btn btn-close btn-sm"

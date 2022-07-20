449550971841afc0a882bffa13810d0e0494c60e
Vandenynas <stlo.subs@gmail.com>
2022-07-20 06:53:21 +0300

    Added token logos and link to Uniswap pool, token balances

--- client/src/components/PriceChart.js
+++ client/src/components/PriceChart.js
@@ -2,89 +2,123 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import Chart from "react-apexcharts";
 import TimeframeSelector from './TimeframeSelector';
+import TokenBalances from './TokenBalances';
 import { 
   fetchPriceData,
   priceDataSelector,
+  scrollGraph
 } from '../slices/priceData';
 import {indicatorConfigSelector} from '../slices/indicatorConfig';
+import {accountsSelector, loadBalances} from '../slices/accounts';
 
 import {
   calculateCandlestickData,
   getChartingData
 } from '../helpers/priceDataCalculator';
 import {defaultChartOptions} from '../helpers/chartOptions';
+import MetaMaskOnboarding from '@metamask/onboarding';
+
 
 // Temporary parameters derived from user interaction
 const userSymbol = 'AAVEWETH';
 
 const PriceChart = () => {
   const dispatch = useDispatch();
+
   const {
     loading,
     hasErrors,
     priceObject,
     chartObject,
     viewTimeframe,
-    maxCandlesNumber,
-    indicatorColors
   } = useSelector(priceDataSelector);
+
+  const { 
+    provider,
+    account,
+    baseTokenContract,
+    quoteTokenContract,
+    baseTokenBalance,
+    quoteTokenBalance,
+    balancesLoading
+  } = useSelector(accountsSelector);
+
+  console.log(provider, account)
 
   const arrayOHLC = priceObject.arrayOHLC;
 
   const {configuringIndicator, currentIndicator} = useSelector(indicatorConfigSelector);
 
+  const indicatorColors = [''];
+
   useEffect(() => {
     dispatch(fetchPriceData(userSymbol, viewTimeframe));
+    // const {baseToken, quoteToken} = priceObject;
+    // dispatch(loadTokenContracts(baseToken.address, quoteToken.address));
+    // dispatch(loadBalances(account, baseTokenContract, quoteTokenContract));
   }, [dispatch]);
 
+  useEffect(() => {
+    getBalances();
+  }, [priceObject]);
 
-  const renderChart = () => {
-    if(loading) return <p>Loading price data...</p>
-    if(hasErrors) return <p>Unable to display chart</p>
-
-    const candleData = calculateCandlestickData(arrayOHLC);
-
-    const priceSeries = [
-      {
-        id: 1,
-        name: chartObject.series[0].name,
-        type: 'candlestick',
-        data: candleData
-      }
-    ];
-
-    // Checking if there are moving average objects in the chartObject series
-    for(let series of chartObject.series) {
-      if(series.id > 1) {
-
-        // If the current series belongs to an indicator that is being configured,
-        // the use the currentIndicator state variable
-        // Otherwise use the default configuration stored in the state
-        if(configuringIndicator && currentIndicator.id === series.id) {
-          series = currentIndicator;    
-        }
-
-        const {name, type, data, visible} = series;    
-
-        const chartingData = visible ? getChartingData(data, arrayOHLC) : [];
-
-        const movingAverageObject = {
-          name: name,
-          type: type,
-          data: chartingData
-        }
-
-        priceSeries.push(movingAverageObject);
-      }
+  const getBalances = () => {
+    const {baseToken, quoteToken} = priceObject;
+    if(baseToken !== {} && quoteToken !== {}) {
+      dispatch(loadBalances('0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5', quoteToken.address));
     }
+  }
 
-    // Checking if there is an indicator being configured in the user dialog
-    // This section only renders new indicators, not the existing ones that are being configured
-    if (configuringIndicator && currentIndicator.id === 0) {
+  useEffect(() => {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      window.ethereum.on('accountsChanged', getBalances);
+      return () => {
+        window.ethereum.removeListener('accountsChanged', getBalances)
+      };
+    }
+  }, []);
 
-      const { name, type, data} = currentIndicator;
 
-      const chartingData = getChartingData(data, arrayOHLC);
+
+  console.log('baseToken', baseTokenContract, quoteTokenContract);
+
+  console.log('balance', baseTokenBalance, quoteTokenBalance);
+
+
+  const handleScroll = (event) => {
+    event.preventDefault();
+    if(event.deltaY < 0) {
+      dispatch(scrollGraph(-1));
+    } else if (event.deltaY > 0) {
+      dispatch(scrollGraph(1));
+    }
+  }
+
+  const candleData = calculateCandlestickData(arrayOHLC);
+
+  const priceSeries = [
+    {
+      id: 1,
+      name: chartObject.series[0].name,
+      type: 'candlestick',
+      data: candleData
+    }
+  ];
+
+  // Checking if there are moving average objects in the chartObject series
+  for(let series of chartObject.series) {
+    if(series.id > 1) {
+
+      // If the current series belongs to an indicator that is being configured,
+      // the use the currentIndicator state variable
+      // Otherwise use the default configuration stored in the state
+      if(configuringIndicator && currentIndicator.id === series.id) {
+        series = currentIndicator;    
+      }
+
+      const {name, type, data, visible, color} = series;    
+
+      const chartingData = visible ? getChartingData(data, arrayOHLC) : [];
 
       const movingAverageObject = {
         name: name,
@@ -93,33 +127,81 @@ const PriceChart = () => {
       }
 
       priceSeries.push(movingAverageObject);
-    } 
+      indicatorColors.push(color);
+    }
+  }
 
+  // Checking if there is an indicator being configured in the user dialog
+  // This section only renders new indicators, not the existing ones that are being configured
+  if (configuringIndicator && currentIndicator.id === 0) {
 
-    const chartOptionsXmax = candleData[candleData.length -1][0];
-    const chartOptionsXmin = candleData[Math.max(0, candleData.length - maxCandlesNumber)][0];
+    const { name, type, data, color} = currentIndicator;
+
+    const chartingData = getChartingData(data, arrayOHLC);
+
+    const movingAverageObject = {
+      name: name,
+      type: type,
+      data: chartingData
+    }
+
+    priceSeries.push(movingAverageObject);
+    indicatorColors.push(color);
+  }
+
+  const renderChart = () => {
+    if(loading) return <p>Loading price data...</p>
+    if(hasErrors) return <p>Unable to display chart</p>
 
     const priceChart = {
       symbol: priceObject.symbol,
-      options: defaultChartOptions(chartOptionsXmin, chartOptionsXmax, indicatorColors),
+      options: defaultChartOptions(chartObject.xMin, chartObject.xMax, indicatorColors),
       series: priceSeries
     }
 
     return (
       <div>
-        <div className="row">
-
-        <h1>{priceObject.symbol}
-        <span>  </span>
-        <img src="https://assets.coingecko.com/coins/images/12645/thumb/AAVE.png?1601374110" style={{height: "80%"}}></img>/
-        <img src="https://assets.coingecko.com/coins/images/2518/thumb/weth.png?1628852295" style={{height: "80%"}}></img></h1>
-        
+        <div className="d-flex w-100 align-items-center justify-content-between">
+            <div className="d-flex align-items-center">
+              <h2 className="p-2 mr-auto">
+                {`${priceObject.baseToken.symbol} / ${priceObject.quoteToken.symbol}`}</h2>
+              <span></span>
+              <a
+                href={`https://etherscan.io/address/${priceObject.baseToken.address.toLowerCase()}`}
+                target="_blank" rel="noreferrer noopener">
+                <img src="https://assets.coingecko.com/coins/images/12645/thumb/AAVE.png?1601374110" style={{height: "100%"}}></img>
+              </a>
+              <a
+                href={`https://etherscan.io/address/${priceObject.quoteToken.address.toLowerCase()}`}
+                target="_blank" rel="noreferrer noopener">
+                <img src="https://assets.coingecko.com/coins/images/2518/thumb/weth.png?1628852295" style={{height: "100%"}}></img>
+              </a>
+            </div>
+            <div>
+              <a className="btn btn-outline-secondary"
+                href={`https://info.uniswap.org/#/pools/${priceObject.poolAddress.toLowerCase()}`}
+                target="_blank" rel="noreferrer noopener">
+              Pool: {`${priceObject.poolAddress.slice(0,6)}...${priceObject.poolAddress.slice(priceObject.poolAddress.length - 4)}`}</a>
+            </div>
         </div>
         <TimeframeSelector />
-        <Chart
-          options={priceChart.options}
-          series={priceChart.series}
-        />
+        <div onWheel={handleScroll}>
+          <Chart
+            options={priceChart.options}
+            series={priceChart.series}
+          />
+        </div>
+        <div>
+          {balancesLoading ? 
+          <h4>Loading balances...</h4> 
+          : 
+          <TokenBalances 
+            baseTokenSymbol={priceObject.baseToken.symbol} 
+            quoteTokenSymbol={priceObject.quoteToken.symbol} 
+            baseTokenBalance={baseTokenBalance} 
+            quoteTokenBalance={quoteTokenBalance} 
+          />}
+        </div>
       </div>
     );
   }

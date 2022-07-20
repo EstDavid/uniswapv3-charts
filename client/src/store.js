acfb4fa656e55c6a906777d33b508b3c64fbe976
Vandenynas <stlo.subs@gmail.com>
2022-07-20 06:58:53 +0300

    Added accounts reducer

--- client/src/store.js
+++ client/src/store.js
@@ -2,11 +2,13 @@ import {configureStore} from '@reduxjs/toolkit';
 import priceDataReducer from './slices/priceData';
 import pairsListsReducer from './slices/pairsLists';
 import indicatorConfigReducer from './slices/indicatorConfig';
+import accountsReducer from './slices/accounts';
 
 export const store = configureStore({
     reducer: {
         priceData: priceDataReducer,
         pairsLists: pairsListsReducer,
-        indicatorConfig: indicatorConfigReducer
+        indicatorConfig: indicatorConfigReducer,
+        accounts: accountsReducer
     }
 });

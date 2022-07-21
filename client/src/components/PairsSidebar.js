a3b67efca53ed0f7b4715109428ca7d9f2a798e2
Vandenynas <stlo.subs@gmail.com>
2022-07-21 21:49:32 +0300

    Added spinner

--- client/src/components/PairsSidebar.js
+++ client/src/components/PairsSidebar.js
@@ -4,12 +4,16 @@ import PairsList from './PairsList';
 import { pairsListsSelector } from '../slices/pairsLists';
 
 const PairsSidebar = () => {
-    const {quoteSymbols} = useSelector(pairsListsSelector);
+    const {quoteSymbols, pairsListsLoading} = useSelector(pairsListsSelector);
 
     return(
         <div className="flex-shrink-0 p-3 bg-white" style={{width: "280px"}}>
             <div className="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
                 <svg className="bi me-2" width="30" height="24"></svg>
+                {pairsListsLoading ? 
+                    <span className="spinner-border spinner-border-sm mr2" role="status" aria-hidden="true"></span>
+                :
+                    <div></div>}
                 <span className="fs-5 fw-semibold">Pool Pairs</span>
             </div>
             <ul className="list-unstyled ps-0">

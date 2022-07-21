4bdf4d9b7fdbba3ab4e916e96bde09a3bd54e9af
Vandenynas <stlo.subs@gmail.com>
2022-07-21 21:48:09 +0300

    Added blockchain selection functionality. Added all useEffect functions from TokenBalances

--- client/src/components/Onboarding.js
+++ client/src/components/Onboarding.js
@@ -1,17 +1,25 @@
 import React, { useEffect } from 'react';
 import MetaMaskOnboarding from '@metamask/onboarding';
 import { useDispatch, useSelector } from 'react-redux';
-import { accountsSelector, checkProvider, loadAccount } from '../slices/accounts';
+import { priceDataSelector } from '../slices/priceData';
+import { accountsSelector, checkProvider, loadBalances, loadProvider, loadAccount } from '../slices/accounts';
+import { checkBlockchain, requestBlockchain, blockchainSelector } from '../slices/blockchain';
 
-const ONBOARD_TEXT = 'Click here to install MetaMask';
-const CONNECT_TEXT = 'Connect Metamask';
-
-export function OnboardingButton() {
+const OnboardingButton = (props) => {
 
   const dispatch = useDispatch();
 
+  const { priceObject } = useSelector(priceDataSelector);
+  const { baseToken, quoteToken, symbol } = priceObject;
+
   const {account, metamaskInstalled} = useSelector(accountsSelector);
 
+  const { currentBlockchainId, defaultBlockchain, defaultBlockchainActive } = useSelector(blockchainSelector);
+  
+  const ONBOARD_TEXT = "Click here to install Metamask";
+  const CONNECT_TEXT = "Connect Metamask";
+  const SWITCH_TEXT = `Switch to ${defaultBlockchain.chainName}`;
+  
   const onboarding = new MetaMaskOnboarding();
 
   let buttonText;
@@ -19,6 +27,8 @@ export function OnboardingButton() {
 
   if(!metamaskInstalled) {
     buttonText = ONBOARD_TEXT;
+  } else if (!defaultBlockchainActive && account !== undefined) {
+    buttonText = SWITCH_TEXT;
   } else if (account !== undefined) {
     buttonText = `${account.slice(0,6)}...${account.slice(account.length - 4)}`;
     isDisabled = true;
@@ -27,13 +37,51 @@ export function OnboardingButton() {
     buttonText = CONNECT_TEXT;
   }
 
-  const handleNewAccount = () => {
-    const newAccount = dispatch(loadAccount());
+  const handleNewAccount = async () => {
+    const provider = await dispatch(loadProvider());
+    await dispatch(loadAccount(provider));
+    const id = await dispatch(checkBlockchain(provider));
+    if(!defaultBlockchainActive) {
+      dispatch(requestBlockchain(provider, defaultBlockchain));
+    }
+  }
+
+  // const handleNewChain = async () => {
+
+  // }
+
+  const getBalances = () => {
+    if(baseToken !== {} && quoteToken !== {} && defaultBlockchainActive) {
+      dispatch(loadBalances(baseToken, quoteToken, account));
+    }
   }
 
   useEffect(() => {
     dispatch(checkProvider());
   }, []);
+
+  useEffect(() => {
+    if (metamaskInstalled) {
+      window.ethereum.on('accountsChanged', handleNewAccount);
+      window.ethereum.on('chainChanged', handleNewAccount);
+      return () => {
+        window.ethereum.removeListener('accountsChanged', handleNewAccount);
+        window.ethereum.removeListener('chainChanged', handleNewAccount);
+      };
+    }
+  }, [symbol]);
+
+  useEffect(() => {
+    if(account !== undefined) {
+        getBalances();
+    }
+  }, [account, symbol]);
+
+  useEffect(() => {
+      if(account !== undefined) {
+          getBalances();
+      }
+  }, [defaultBlockchainActive]);
 
   const onClick = () => {
     if (metamaskInstalled) {
@@ -43,8 +91,10 @@ export function OnboardingButton() {
     }
   };
   return (
-    <button className="btn btn-warning alert-button" disabled={isDisabled} onClick={onClick}>
+    <button className="btn btn-warning alert-button me-lg-3" disabled={isDisabled} onClick={onClick}>
       {buttonText}
     </button>
   );
 }
+
+export default OnboardingButton;

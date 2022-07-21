e69fc521b797adbb92689340bc8170154b8812df
Vandenynas <stlo.subs@gmail.com>
2022-07-21 21:46:19 +0300

    Added TokenBalances

--- client/src/components/IndicatorSidebar.js
+++ client/src/components/IndicatorSidebar.js
@@ -3,11 +3,20 @@ import { useSelector } from 'react-redux';
 import IndicatorButton from './IndicatorButton';
 import IndicatorSettings from './IndicatorSettings';
 import IndicatorList from './IndicatorList';
+import TokenBalances from './TokenBalances';
+import { accountsSelector } from '../slices/accounts';
+import { blockchainSelector } from '../slices/blockchain';
 
 const IndicatorSidebar = () => {
 
+    const { account, metamaskInstalled } = useSelector(accountsSelector);
+
+    const { defaultBlockchainActive } = useSelector(blockchainSelector);
+
     return(
         <div className="flex-shrink-0 p-3 bg-white" style={{width: "280px"}}>
+            {!metamaskInstalled || account === undefined || !defaultBlockchainActive ? 
+            <div></div> : <TokenBalances />}
             <div className="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
                 <svg className="bi me-2" width="30" height="24"></svg>
                 <span className="fs-5 fw-semibold">Indicators</span>

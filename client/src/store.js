6b6f9afd02590d8241d47dc7d8899de38dcf1c29
Vandenynas <stlo.subs@gmail.com>
2022-07-21 21:55:03 +0300

    Added blockchain reducer

--- client/src/store.js
+++ client/src/store.js
@@ -3,12 +3,14 @@ import priceDataReducer from './slices/priceData';
 import pairsListsReducer from './slices/pairsLists';
 import indicatorConfigReducer from './slices/indicatorConfig';
 import accountsReducer from './slices/accounts';
+import blockchainReducer from './slices/blockchain';
 
 export const store = configureStore({
     reducer: {
         priceData: priceDataReducer,
         pairsLists: pairsListsReducer,
         indicatorConfig: indicatorConfigReducer,
-        accounts: accountsReducer
+        accounts: accountsReducer,
+        blockchain: blockchainReducer
     }
 });

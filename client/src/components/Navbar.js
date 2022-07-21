3637b98ef966659d29ede1bb00e33fd067dbbe5a
Vandenynas <stlo.subs@gmail.com>
2022-07-21 21:47:12 +0300

    Relocated OnboardingButton. Added brand name

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { OnboardingButton } from './Onboarding';
+import OnboardingButton from './Onboarding';
 import { filterInput, pairsListsSelector } from '../slices/pairsLists';
 import { fetchPriceData, priceDataSelector} from '../slices/priceData';
 
@@ -13,7 +13,6 @@ const Navbar = () => {
   const {
     viewTimeframe
   } = useSelector(priceDataSelector);
-
 
   const {
     pairsLists,
@@ -36,50 +35,43 @@ const filterSymbols = (event) => {
 }
 
   return(
-      <header className="p-3 bg-dark text-white">
-      <div className="container">
-        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-            <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
-          </a>
-  
-          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
-            <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
-          </ul>
-  
-          <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-            <input 
-              type="search"
-              className="form-control form-control-dark"
-              placeholder="Search symbol..."
-              aria-label="Search"
-              id="dropdown-results" data-bs-toggle="dropdown" aria-expanded="false"
-              onChange={filterSymbols}
-              value={searchInputs[symbolAll]}
-              />
-            <ul className="dropdown-menu" aria-labelledby="dropdown-results">
-                {searchPairsLists[symbolAll].map((symbol, index) => {
-                    return <li key={index}>
-                              <a href="#" className="dropdown-item link-dark rounded" onClick={selectSymbol}>{symbol}</a>
-                            </li>
-                })}
-            </ul>
-          </form>
-  
-          <div className="text-end">
-            <a 
-              role="button"
-              className="btn btn-outline-light me-2"
-              href="https://github.com/Vandenynas/uniswapv3-charts"
-              target="_blank"
-              rel="noreferrer noopener"
-            ><i className="bi bi-github"></i>View on Github</a>
-            <OnboardingButton />
+      <nav className="p-3 bg-dark text-white">            
+        <div className="container-fluid">
+          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" className="navbar-brand text-white text-decoration-none me-lg-auto">
+              <span>UniCharts</span>
+            </a>
+            <OnboardingButton />  
+            <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+              <input 
+                type="search"
+                className="form-control form-control-dark"
+                placeholder="Search symbol..."
+                aria-label="Search"
+                id="dropdown-results" data-bs-toggle="dropdown" aria-expanded="false"
+                onChange={filterSymbols}
+                value={searchInputs[symbolAll]}
+                />
+              <ul className="dropdown-menu" aria-labelledby="dropdown-results">
+                  {searchPairsLists[symbolAll].map((symbol, index) => {
+                      return <li key={index}>
+                                <a href="#" className="dropdown-item link-dark rounded" onClick={selectSymbol}>{symbol}</a>
+                              </li>
+                  })}
+              </ul>
+            </form>
+            <div className="text-end">
+              <a 
+                role="button"
+                className="btn btn-outline-light me-2"
+                href="https://github.com/Vandenynas/uniswapv3-charts"
+                target="_blank"
+                rel="noreferrer noopener"
+              ><i className="bi bi-github"></i>View on Github</a>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+    </nav>
   )
 }
 

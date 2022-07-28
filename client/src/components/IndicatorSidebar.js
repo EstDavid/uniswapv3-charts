44564d4a290c0aeec9406583598e903e2b8dd4d4
Vandenynas <stlo.subs@gmail.com>
2022-07-28 19:22:35 +0300

    Deleted link-dark class

--- client/src/components/IndicatorSidebar.js
+++ client/src/components/IndicatorSidebar.js
@@ -17,7 +17,7 @@ const IndicatorSidebar = () => {
         <div className="flex-shrink-0 p-3 bg-white" style={{width: "280px"}}>
             {!metamaskInstalled || account === undefined || !defaultBlockchainActive ? 
             <div></div> : <TokenBalances />}
-            <div className="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+            <div className="d-flex align-items-center pb-3 mb-3 text-decoration-none border-bottom">
                 <svg className="bi me-2" width="30" height="24"></svg>
                 <span className="fs-5 fw-semibold">Indicators</span>
             </div>

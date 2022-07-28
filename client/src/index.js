9c5ad3f9a3e86d37ec9a4cd92901cc1df3564728
Vandenynas <stlo.subs@gmail.com>
2022-07-28 19:32:57 +0300

    Added bootsrtap css and replaced index.css import for the index.scss sass file

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
 import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';

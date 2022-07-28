08fa6e9411d4743b7125139a8894c39ae60fa2ab
Vandenynas <stlo.subs@gmail.com>
2022-07-28 19:24:04 +0300

    Implemented responsive classes. Implemented primary class with overridden color

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -35,15 +35,20 @@ const filterSymbols = (event) => {
 }
 
   return(
-      <nav className="p-3 bg-dark text-white">            
-        <div className="container-fluid">
-          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="/" className="navbar-brand text-white text-decoration-none me-lg-auto">
-              <span>UniCharts</span>
-            </a>
-            <OnboardingButton />  
-            <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-              <input 
+    <nav className="navbar navbar-expand-md navbar-primary bg-primary text-white pb-2" aria-label="UniCharts navbar">
+      <div className="container-fluid">
+        <a href="/" className="navbar-brand text-white text-decoration-none me-md-auto">
+          <span>UniCharts</span>
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu" aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbar-menu">
+          <div className="navbar-nav ms-auto flex-nowrap mt-3 mt-md-0">
+            <OnboardingButton />
+            <form className="nav-item dropdown col-md-auto mb-3 mb-md-0 me-md-3">
+              <input
                 type="search"
                 className="form-control form-control-dark"
                 placeholder="Search symbol..."
@@ -51,27 +56,26 @@ const filterSymbols = (event) => {
                 id="dropdown-results" data-bs-toggle="dropdown" aria-expanded="false"
                 onChange={filterSymbols}
                 value={searchInputs[symbolAll]}
-                />
+              />
               <ul className="dropdown-menu" aria-labelledby="dropdown-results">
-                  {searchPairsLists[symbolAll].map((symbol, index) => {
-                      return <li key={index}>
-                                <a href="#" className="dropdown-item link-dark rounded" onClick={selectSymbol}>{symbol}</a>
-                              </li>
-                  })}
+                {searchPairsLists[symbolAll].map((symbol, index) => {
+                  return <li key={index}>
+                    <a href="#" className="dropdown-item rounded" onClick={selectSymbol}>{symbol}</a>
+                  </li>
+                })}
               </ul>
             </form>
-            <div className="text-end">
-              <a 
-                role="button"
-                className="btn btn-outline-light me-2"
-                href="https://github.com/Vandenynas/uniswapv3-charts"
-                target="_blank"
-                rel="noreferrer noopener"
-              ><i className="bi bi-github"></i>View on Github</a>
-            </div>
+            <a
+              role="button"
+              className="btn btn-outline-light"
+              href="https://github.com/Vandenynas/uniswapv3-charts"
+              target="_blank"
+              rel="noreferrer noopener"
+            ><i className="bi bi-github"></i>View on Github</a>
           </div>
         </div>
-    </nav>
+      </div>
+    </nav> 
   )
 }
 

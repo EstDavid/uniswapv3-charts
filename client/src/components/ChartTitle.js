853f92ae76ef0e9d8d2d428ec1249e3f2718e1cf
Vandenynas <stlo.subs@gmail.com>
2022-08-04 10:03:33 +0300

    Refactored a elements for the symbols into a function. Eliminated borders

--- client/src/components/ChartTitle.js
+++ client/src/components/ChartTitle.js
@@ -2,6 +2,24 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import {priceDataSelector} from '../slices/priceData';
 
+const TokenDisplay = (props) => {
+    return(
+        <a
+            className="list-group-item px-1 text-reset border-0"
+            href={`https://etherscan.io/address/${props.address.toLowerCase()}`}
+            target="_blank" rel="noreferrer noopener">
+            <div className="d-flex align-items-center">
+                {props.showLogos ? 
+                    <img className="me-1 charttitle-logo" src={props.logoURI}></img>
+                :
+                <div></div>}
+                <h2 className="w-100">{`${props.symbol}`}</h2>
+            </div>
+        </a>
+    )
+
+}
+
 const ChartTitle = () => {
 
     const { priceObject } = useSelector(priceDataSelector);
@@ -18,31 +36,19 @@ const ChartTitle = () => {
     return (
         <div className="d-flex w-100 align-items-center justify-content-between">
             <div className="list-group list-group-horizontal list-group-flush">
-                <a
-                    className="list-group-item px-1 text-reset"
-                    href={`https://etherscan.io/address/${priceObject.baseToken.address.toLowerCase()}`}
-                    target="_blank" rel="noreferrer noopener">
-                    <div className="d-flex align-items-center">
-                        {showLogos ? 
-                            <img className="me-1 charttitle-logo" src={baseLogo}></img>
-                        :
-                        <div></div>}
-                        <h2 className="w-100">{`${priceObject.baseToken.symbol}`}</h2>
-                    </div>
-                </a>                
-                <a className="list-group-item px-0 text-reset"><h2>/</h2></a>
-                <a
-                    className="list-group-item px-1 text-reset"
-                    href={`https://etherscan.io/address/${priceObject.quoteToken.address.toLowerCase()}`}
-                    target="_blank" rel="noreferrer noopener">
-                    <div className="d-flex align-items-center">
-                        {showLogos ? 
-                            <img className="me-1 charttitle-logo" src={quoteLogo}></img>
-                        :
-                        <div></div>}
-                        <h2 className="w-100">{`${priceObject.quoteToken.symbol}`}</h2>                            
-                    </div>
-                </a>
+                <TokenDisplay 
+                    address={priceObject.baseToken.address} 
+                    symbol={priceObject.baseToken.symbol}
+                    showLogos={showLogos}
+                    logoURI={baseLogo} 
+                />                
+                <a className="list-group-item px-0 text-reset border-0"><h2>/</h2></a>
+                <TokenDisplay 
+                    address={priceObject.quoteToken.address} 
+                    symbol={priceObject.quoteToken.symbol}
+                    showLogos={showLogos}
+                    logoURI={quoteLogo} 
+                /> 
             </div>
             <div>
             <a className="btn btn-outline-secondary"
